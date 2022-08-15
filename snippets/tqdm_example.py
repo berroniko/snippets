dd664c1bb115af3103975c14746a039d70d1e28e
berroniko <66038207+berroniko@users.noreply.github.com>
2022-08-15 21:36:20 +0200

    Update tqdm_example.py

--- snippets/tqdm_example.py
+++ snippets/tqdm_example.py
@@ -1,7 +1,10 @@
 from tqdm import tqdm
 from time import sleep
 
-"""Example for using tqdm"""
+"""
+Example for using tqdm
+Instantly make loops show a smart progress meter - just wrap any iterable with tqdm(iterable)
+"""
 
 r = 0
 for i in tqdm(range(1000)):
