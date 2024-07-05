86c0eb589a901f8f74a70ed55e84274d586e2608
berroniko <buzones-git@yahoo.com>
2024-07-05 17:16:37 +0200

    (property.py) updated

--- snippets/property.py
+++ snippets/property.py
@@ -1,26 +1,46 @@
-
-class Test():
-    def __init__(self, first_nr):
-        self.first = first_nr
-        self._result = None
-
-    def calculation(self):
-        print("calculating")
-        self._result = self.first * 5
-        return self._result
-
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     @property
-    def result(self):
-        print("input property")
-        if not self._result:
-            self.calculation()
-        return self._result
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if value <= 0:
+            raise ValueError("Width must be positive")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if value <= 0:
+            raise ValueError("Height must be positive")
+        self._height = value
+
+    @property
+    def area(self):
+        return self._width * self._height
+
+    def weight(self, density):
+        return self.area * density
 
 
 if __name__ == '__main__':
+    rect = Rectangle(width=5, height=3)
+    print("before assignment")
+    print(f"{rect.width=}")
+    print(f"{rect.area=}")
 
-    t = Test(2)
-    print(t.result)
-    print(t.result)
-    print(t.calculation())
+    rect.width = 10
+    print("\nafter assignment ")
+    print(f"{rect.width=}")
+    print(f"{rect.area=}")
+
+    print("\ninvalid initialization as well as settings raise an error:\n")
+    r = Rectangle(width=2, height=-5)
